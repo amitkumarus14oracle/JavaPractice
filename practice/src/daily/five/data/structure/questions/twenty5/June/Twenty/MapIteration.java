9cb7976aa656c1019fdcb583df652e4515deca33
Amit Kumar <amitkumarus14jan@gmail.com>
2020-06-26 13:59:48 +0530

    changed Mytest file name to MapIteration

--- practice/src/daily/five/data/structure/questions/twenty5/June/Twenty/MapIteration.java
+++ practice/src/daily/five/data/structure/questions/twenty5/June/Twenty/MapIteration.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
-public class MyTest {
+public class MapIteration {
     public static void main(String[] args) {
         HashMap<String, String> h = new HashMap<>();
         h.put("amit", "amit1");
